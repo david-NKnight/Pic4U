a689101757d18c549e18086d3166085f734f4e55
David Knight <111792945+david-NKnight@users.noreply.github.com>
2023-04-01 12:37:54 -0500

    Update Pic4U.js

--- Pic4U.js
+++ Pic4U.js
@@ -1,47 +1,31 @@
-filterSelection("all") // Execute the function and show all columns
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("column");
-  if (c == "all") c = "";
-  // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
+// Get all the gallery-item elements
+const galleryItems = document.querySelectorAll('.gallery-item');
 
-// Show filtered elements
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {
-      element.className += " " + arr2[i];
+// Loop through the gallery-item elements and add an event listener for when the image loads
+galleryItems.forEach(item => {
+  const img = item.querySelector('img');
+  img.addEventListener('load', () => {
+    // Get the image's natural width and height
+    const naturalWidth = img.naturalWidth;
+    const naturalHeight = img.naturalHeight;
+
+    // Calculate the aspect ratio
+    const aspectRatio = naturalWidth / naturalHeight;
+
+    // Get the width and height of the gallery-item element
+    const itemWidth = item.offsetWidth;
+    const itemHeight = item.offsetHeight;
+
+    // Calculate the new width and height of the image
+    let newWidth, newHeight;
+    if (aspectRatio > 1) {
+      // Landscape orientation
+      newWidth = itemWidth;
+      newHeight = itemWidth / aspectRatio;
+    } else {
+      // Portrait or square orientation
+      newHeight = itemWidth / aspectRatio;
+      newWidth = itemWidth;
     }
-  }
-}
-
-// Hide elements that are not selected
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);
-    }
-  }
-  element.className = arr1.join(" ");
-}
-
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
   });
-}
+});
